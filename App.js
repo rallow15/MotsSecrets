c17cdba44ba532c2dae0081accffc5390f4cd1b4
Salim <salimchamsdine@gmail.com>
2026-07-09 22:15:33 +0200

    fix: remove gesture-handler import causing iOS black screen

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import 'react-native-gesture-handler';
 import { View, Text, ActivityIndicator, StyleSheet, Platform, NativeModules } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
